9e6fafc752d0350aedd4e133d6bea82222b434fd
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-06-06 00:46:40 +0530

    Create 42) Aggressive Cows.cpp

---
+++ 42) Aggressive Cows.cpp
@@ -0,0 +1,53 @@
+You are given an array with unique elements of stalls[], which denote the positions of stalls. You are also given an integer k which denotes the number of aggressive cows. The task is to assign stalls to k cows such that the minimum distance between any two of them is the maximum possible.
+
+Examples:
+
+Input: stalls[] = [1, 2, 4, 8, 9], k = 3
+Output: 3
+Explanation: The first cow can be placed at stalls[0], 
+the second cow can be placed at stalls[2] and 
+the third cow can be placed at stalls[3]. 
+The minimum distance between cows in this case is 3, which is the largest among all possible ways.
+
+
+class Solution {
+  public:
+    int check(int mid, int k, vector<int>& nums){
+        int n = nums.size();
+        
+        int count = 1;
+        int last = nums[0];
+        
+        for(int i=1;i<n;i++){
+            if(nums[i] - last >= mid){
+                count++;
+                last = nums[i];
+            }
+        }
+        
+        return count >= k;
+    }
+    int aggressiveCows(vector<int> &nums, int k) {
+        // code here
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        
+        int lo = 1;
+        int hi = *max_element(nums.begin(),nums.end());
+        
+        int ans = INT_MIN;
+        
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            
+            if(check(mid,k,nums)){
+                ans = mid;
+                lo = mid + 1;
+            }
+            else hi = mid - 1;
+        }
+        
+        return ans;
+        
+    }
+};
