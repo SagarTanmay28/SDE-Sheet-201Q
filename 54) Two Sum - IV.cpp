8c0ab6c914a5e2ff6ce22ddabc11e26ce24958aa
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-06-06 01:49:02 +0530

    Create 54) Two Sum - IV.cpp

---
+++ 54) Two Sum - IV.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool check(TreeNode* root, int k, unordered_set<int>& st){
+        if(root == NULL) return false;
+        if(st.find(k - root->val) != st.end()) return true;
+        st.insert(root->val);
+        return check(root->left,k,st) || check(root->right,k,st);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int> st;
+        return check(root,k,st);
+    }
+};
