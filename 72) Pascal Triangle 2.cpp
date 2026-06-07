1d905a56833432374ab840187cc997a447aa90ca
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-06-07 22:40:56 +0530

    Create 72) Pascal Triangle 2.cpp

---
+++ 72) Pascal Triangle 2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        
+       vector<int> v(rowIndex+1,1);
+       
+        for(int i=1;i<=rowIndex;i++){
+
+            for(int j=i-1;j>0;j--){
+               v[j] = v[j] + v[j-1]; 
+            }
+        
+        }
+
+        return v; 
+        
+    }
+};
